c4c0519b764f37651dc7ec5ec1ca6f4d88de7fbc
Maksymilian Komarnicki <maksymilian.komarnicki@wp.pl>
2025-01-27 21:02:00 +0100

    Install get frame and get camera config callbacks on SERVER

--- streamer/server/espfsp_comm_proto_conf.c
+++ streamer/server/espfsp_comm_proto_conf.c
@@ -61,6 +61,8 @@ esp_err_t espfsp_server_comm_protos_init(espfsp_server_instance_t *instance)
     config.req_callbacks[ESPFSP_COMM_REQ_FRAME_SET_PARAMS] = espfsp_server_req_frame_set_params_handler;
     config.req_callbacks[ESPFSP_COMM_REQ_SOURCE_SET] = espfsp_server_req_source_set_handler;
     config.req_callbacks[ESPFSP_COMM_REQ_SOURCE_GET] = espfsp_server_req_source_get_handler;
+    config.req_callbacks[ESPFSP_COMM_REQ_FRAME_GET_PARAMS] = espfsp_server_req_frame_get_params_handler;
+    config.req_callbacks[ESPFSP_COMM_REQ_CAM_GET_PARAMS] = espfsp_server_req_cam_get_params_handler;
     config.repetive_callback = NULL;
     config.repetive_callback_freq_us = 100000000;
     config.conn_closed_callback = espfsp_server_connection_stop;
